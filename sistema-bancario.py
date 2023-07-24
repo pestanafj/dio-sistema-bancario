0c3bfd268acbcd7a56ff1aed1c83cd9e1314f595
Fernanda Pestana <138170962+pestanafj@users.noreply.github.com>
2023-07-24 15:28:59 -0300

    Revert "Feature/client registration"

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -4,260 +4,80 @@ from datetime import datetime
 import sys, os
 
 
-def menu_principal(clientes, contas):
+# transacoes = dict()
+
+selected_option = -1
+account_balance = 0
+statement = ""
+count_loot = 0
+
+LOOT_LIMIT = 500
+NUMBER_LOOT = 3
+
+
+#######################################################
+#    MENU PRINCIPAL
+#######################################################
+
+
+def print_main_menu():
     os.system("cls")
 
     print(
-        """ 
---------------------------------------------
-                PESTANA BANK
---------------------------------------------
+        """
+------------------------------------------
+                  MENU                  
+------------------------------------------
 
-[1] - Já sou cliente
-[2] - Desejo ser cliente
-[3] - Atendimento sem cadastro
-[9] - Finalizar atendimento
+    [0] - Depósito
+    [1] - Saque
+    [2] - Extrato
+    [9] - Sair
 
---------------------------------------------
-    """
+------------------------------------------
+------------------------------------------
+        """
     )
 
-    opcaoSelecionada = input("Selecione a opção desejada: ")
 
-    if opcaoSelecionada == "1":
-        print("\n>>> Já sou cliente!\n")
-        atendimento_cliente(clientes, contas)
-    elif opcaoSelecionada == "2":
-        print("\n>>> Desejo ser cliente!!\n")
-        cadastrar_cliente(clientes, contas)
-    elif opcaoSelecionada == "3":
-        atendimento_sem_cadastro(clientes, contas)
-    elif opcaoSelecionada == "9":
-        sair_do_programa()
-    else:
-        input("\n!!! Opção Inválida.\nPressione qualquer tecla para continuar...\n")
-        menu_principal(clientes, contas)
-
-    return clientes, contas
+#########################################################
 
 
-def menu_rodape(clientes, contas, menu_de_chamada, outra_operacao=False):
-    str_operacao = ""
-    if outra_operacao:
-        str_operacao = "\n[#] - Fazer outra operação"
+#########################################################
+#   MENU AUXILIAR
+#########################################################
 
-    print(
-        f"""
-\n------------------------------------------{str_operacao}
-[0] - Voltar ao Início
-[9] - Sair\n
+
+def aux_menu():
+    while True:
+        print(
+            """\n------------------------------------------\n
+    [0] - Voltar ao Menu Principal\n    [9] - Sair\n
 ------------------------------------------\n"""
-    )
+        )
 
-    opcao_selecionada = input("Selecione a opção desejada: ")
+        selected_option = input("   Selecione a opção desejada: ")
 
-    if opcao_selecionada == "9":
-        sair_do_programa()
+        if selected_option == "9":
+            exit_program()
 
-    elif opcao_selecionada == "0":
-        menu_principal(clientes, contas)
+        elif selected_option == "0":
+            break
 
-    elif opcao_selecionada == "#":
-        menu_de_chamada(clientes, contas)
-
-    # elif opcao_selecionada == "*":
-    #     menu_anterior(clientes, contas)
-
-    else:
-        print("\n!!! Opção inválida!")
-        menu_rodape(clientes, contas, menu_de_chamada, outra_operacao)
-
-    return clientes, contas
+        else:
+            print("\n   Opção inválida!")
+            continue
 
 
-def atendimento_cliente(clientes, contas, cpf_cliente=""):
-    os.system("cls")
-
-    print(
-        """
---------------------------------------------
-           PESTANA BANK - CLIENTE           
---------------------------------------------"""
-    )
-
-    # if cpf_cliente == "":
-    cpf = input("Digite o CPF: ")
-
-    for digito in cpf:
-        if digito >= "0" and digito <= "9":
-            cpf_cliente += digito
-
-    if len(cpf_cliente) != 11:
-        print("\n!!! Erro!\n!!! CPF inválido!\n")
-        menu_rodape(clientes, contas, atendimento_cliente, True)
-
-    index_cliente = buscar_cliente(clientes, "cpf", cpf_cliente)
-
-    if index_cliente == -1:
-        print("\n!!! Erro!\n!!! Não existe cliente cadastrado com esse CPF!\n")
-        menu_rodape(clientes, contas, atendimento_cliente, True)
-
-    cliente = clientes[index_cliente]
-    nome_cliente = cliente["nome"]
-
-    print(f"\n\n>>> Bem vindo {nome_cliente}!")
-
-    print(
-        """
-
-[1] - Criar Conta
-[2] - Listar Contas
-[3] - Depósito
-[4] - Saque
-[5] - Extrato
-[0] - Voltar ao Menu Anterior
-[9] - Finalizar atendimento
-
---------------------------------------------
-"""
-    )
-
-    opcaoSelecionada = input("Selecione a opção desejada: ")
-
-    if opcaoSelecionada == "1":
-        print("\n>>> Criar conta corrente!\n")
-
-        # criar_conta_corrente(clientes, contas, cpf_cliente)
-    elif opcaoSelecionada == "2":
-        print("\n>>> Listar contas!\n")
-
-        # listar_contas(contas, cpf_cliente)
-    elif opcaoSelecionada == "3":
-        print("\n>>> Depositar!\n")
-        depositar(clientes, contas, cpf_cliente=cpf_cliente)
-    elif opcaoSelecionada == "4":
-        print("\n>>> Saque\n")
-    elif opcaoSelecionada == "5":
-        print("\n>>> Imprimir extrato\n")
-        # imprimir_extrato(clientes, contas)
-    elif opcaoSelecionada == "0":
-        menu_principal(clientes, contas)
-    elif opcaoSelecionada == "9":
-        sair_do_programa()
-    else:
-        input("\n!!! Opção Inválida.\nPressione qualquer tecla para continuar...\n")
-        atendimento_cliente(clientes, contas)
-
-    menu_rodape(clientes, contas, atendimento_cliente, True)
-
-    return clientes, contas
+#########################################################
 
 
-def atendimento_sem_cadastro(clientes, contas):
-    os.system("cls")
-
-    print(
-        """
---------------------------------------------
-  PESTANA BANK - ATENDIMENTO SEM CADASTRO           
---------------------------------------------
-
-[1] - Listar Clientes
-[2] - Listar Contas
-[3] - Depósito
-[4] - Extrato
-[0] - Voltar ao Menu Principal
-[9] - Finalizar o Atendimento
-
---------------------------------------------
-"""
-    )
-
-    opcaoSelecionada = input("Selecione a opção desejada: ")
-
-    if opcaoSelecionada == "1":
-        print("\n>>> Listar clientes!")
-        # listar_clientes(clientes)
-        menu_rodape(clientes, contas, atendimento_sem_cadastro, True)
-    elif opcaoSelecionada == "2":
-        print("\n>>> Listar Contas")
-        # listar_contas(contas)
-        menu_rodape(clientes, contas, atendimento_sem_cadastro, True)
-    elif opcaoSelecionada == "3":
-        print("\n>>> Depositar!\n")
-
-        # depositar(clientes, contas, "")
-    elif opcaoSelecionada == "4":
-        print("\n>>> Imprimir extrato\n")
-        # imprimir_extrato(clientes, contas)
-    elif opcaoSelecionada == "0":
-        menu_principal(clientes, contas)
-    elif opcaoSelecionada == "9":
-        sair_do_programa()
-    else:
-        input("\n!!! Opção Inválida.\nPressione qualquer tecla para continuar...\n")
-        atendimento_sem_cadastro(clientes, contas)
-
-    menu_rodape(clientes, contas, atendimento_sem_cadastro, True)
+#########################################################
+#   REPETIR OPERACAO
+#########################################################
 
 
-def cadastrar_cliente(clientes, contas):
-    print(
-        """
---------------------------------------------
->>> Cadastrar Cliente
---------------------------------------------"""
-    )
-
-    nome = input("Nome Completo: ")
-    data_de_nascimento = input("Data de Nascimento(dd/mm/aaaa): ")
-    cpf = input("CPF:")
-    print("Endereço:")
-    logradouro = input("Logradouro/rua: ")
-    numero_casa = input("Nº: ")
-    bairro = input("Bairro: ")
-    cidade = input("Cidade: ")
-    estado = input("Estado(XX): ")
-
-    endereco = f"{logradouro}, {numero_casa} - {bairro} - {cidade}/{estado}"
-
-    cpf_tratado = ""
-    for digito in cpf:
-        if digito >= "0" and digito <= "9":
-            cpf_tratado += digito
-
-    print(cpf_tratado)
-
-    if len(cpf_tratado) != 11:
-        print("\n!!! Erro!\n!!! CPF inválido!\n")
-
-    elif buscar_cliente(clientes, "cpf", cpf_tratado) != -1:
-        print("\n!!! Erro!\n!!! Já existe um cadastro para esse CPF!\n")
-
-    else:
-        novo_cliente = {
-            "nome": nome,
-            "data_de_nascimento": data_de_nascimento,
-            "cpf": cpf_tratado,
-            "endereco": endereco,
-        }
-
-        clientes.append(novo_cliente)
-        print("\n\n>>> Cliente cadastrado!")
-
-    menu_rodape(clientes, contas, cadastrar_cliente, True)
-
-    return clientes, contas
-
-
-def buscar_cliente(clientes, chave, valor):
-    for cliente in clientes:
-        if cliente[chave] == valor:
-            return clientes.index(cliente)
-    return -1
-
-
-def repetir_operacao(operation):
+def repeat_operation(operation):
     while True:
         selected_option = input(f"    Deseja fazer outro {operation}? [S/N] ")
 
@@ -272,7 +92,15 @@ def repetir_operacao(operation):
             continue
 
 
-def testar_entrada(str_input):
+#########################################################
+
+
+#########################################################
+#   TESTE DE ENTRADA
+#########################################################
+
+
+def input_test(str_input):
     if len(str_input) >= 4:
         # print("tamanho maior ou igual a 4")
         if str_input[-3] == "." or str_input[-3] == ",":
@@ -286,8 +114,16 @@ def testar_entrada(str_input):
     return 0
 
 
-def depositar():
-    global saldo, extrato
+#########################################################
+
+
+#########################################################
+#   0 - DEPÓSITO
+#########################################################
+
+
+def deposit_value():
+    global account_balance, statement
 
     print(
         """
@@ -299,11 +135,11 @@ def depositar():
     while True:
         print("\n    - formato R$ 00.00")
 
-        str_valor = input("\n    Valor do depósito: R$ ")
+        str_value = input("\n    Valor do depósito: R$ ")
 
-        valor = testar_entrada(str_valor)
+        value = input_test(str_value)
 
-        if valor == 0:
+        if value == 0:
             print("\n    O valor é inválido!")
             continue
 
@@ -311,19 +147,27 @@ def depositar():
             NOW = datetime.now()
             strNOW = NOW.strftime("%d/%m/%y %H:%M")
 
-            saldo += valor
+            account_balance += value
 
-            extrato += f"{strNOW}    Depósito     R$ {valor:,.2f}\n"
+            statement += f"{strNOW}    Depósito     R$ {value:,.2f}\n"
 
             print("    Depósito realizado com sucesso!\n")
 
-            if not repetir_operacao("depósito"):
-                menu_rodape()
+            if not repeat_operation("depósito"):
+                aux_menu()
                 break
 
 
-def sacar():
-    global saldo, extrato, QTD_SAQUES, cont_saques, LIMITE_SAQUE
+#########################################################
+
+
+#########################################################
+#   1 - SAQUE
+#########################################################
+
+
+def take_value():
+    global account_balance, statement, NUMBER_LOOT, count_loot, LOOT_LIMIT
 
     print(
         """
@@ -333,60 +177,68 @@ def sacar():
     )
 
     while True:
-        if cont_saques >= QTD_SAQUES:
+        if count_loot >= NUMBER_LOOT:
             print("\n    Saque não permitido!\n")
             print(
                 """    Você já atingiu sua quantidade
     de saques disponíveis!"""
             )
-            menu_rodape()
+            aux_menu()
             break
 
-        str_valor = input("    Valor do saque: R$ ")
+        str_value = input("    Valor do saque: R$ ")
 
-        valor = testar_entrada(str_valor)
+        value = input_test(str_value)
 
-        if valor == 0:
+        if value == 0:
             print("\n    O valor é inválido!")
             continue
 
         else:
-            #         if cont_saques >= QTD_SAQUES:
+            #         if count_loot >= NUMBER_LOOT:
             #             print("\n    Saque não permitido!\n")
             #             print(
             #                 """    Você já atingiu sua quantidade
             # de saques disponíveis!"""
             #             )
 
-            #             menu_rodape()
+            #             aux_menu()
             #             break
 
-            if saldo < valor:
+            if account_balance < value:
                 print("\n    Saldo insuficiente!\n\n")
-                menu_rodape()
+                aux_menu()
                 break
 
-            elif valor > LIMITE_SAQUE:
+            elif value > LOOT_LIMIT:
                 print("\n    Saque não permitido!\n")
-                print(f"    Seu limite de saque é R${LIMITE_SAQUE},00!\n\n")
+                print(f"    Seu limite de saque é R${LOOT_LIMIT},00!\n\n")
 
             else:
                 NOW = datetime.today()
                 strNOW = NOW.strftime("%d/%m/%y %H:%M")
 
-                saldo -= valor
-                cont_saques += 1
-                extrato += f"{strNOW}    Saque      - R$ {valor:,.2f}\n"
+                account_balance -= value
+                count_loot += 1
+                statement += f"{strNOW}    Saque      - R$ {value:,.2f}\n"
 
                 print("\n    Saque realizado com sucesso!\n\n")
 
-                if not repetir_operacao("saque"):
-                    menu_rodape()
+                if not repeat_operation("saque"):
+                    aux_menu()
                     break
 
 
-def inprimir_extrato():
-    global extrato, saldo
+#########################################################
+
+
+#########################################################
+#   2 - EXTRATO
+#########################################################
+
+
+def print_statement():
+    global statement, account_balance
 
     print(
         """
@@ -395,72 +247,74 @@ def inprimir_extrato():
 ------------------------------------------\n"""
     )
 
-    if extrato == "":
+    if statement == "":
         print("\n\n    Não foram realizadas movimentações\n\n")
     else:
-        print(extrato)
+        print(statement)
 
     print(
         f"""
 ------------------------------------------
-Saldo = R$ {saldo:,.2f}
+Saldo = R$ {account_balance:,.2f}
 ------------------------------------------"""
     )
 
-    menu_rodape()
+    aux_menu()
 
 
-def sair_do_programa():
+#########################################################
+
+
+#########################################################
+#   9 - SAIR DO PROGRAMA
+#########################################################
+
+
+def exit_program():
     print(
         """
---------------------------------------------
-          Obrigado! Volte sempre!                  
---------------------------------------------\n\n\n\n"""
+------------------------------------------
+------------------------------------------
+        Obrigado! Volte sempre!                  
+------------------------------------------
+------------------------------------------\n\n\n\n"""
     )
     sys.exit()
 
 
-def main():
-    clientes = list()
-    contas = list()
-
-    nome = "Fernanda Pestana"
-    data_de_nascimento = "15/10/1988"
-    cpf = "01174820306"
-    logradouro = "Rua 5 (Und 205)"
-    numero_casa = "50"
-    bairro = "Cidade Operária"
-    cidade = "São Luís"
-    estado = "MA"
-
-    endereco = f"{logradouro}, {numero_casa} - {bairro} - {cidade}/{estado}"
-
-    novo_cliente = {
-        "nome": nome,
-        "data_de_nascimento": data_de_nascimento,
-        "cpf": cpf,
-        "endereco": endereco,
-        "contas": [],
-    }
-
-    nova_conta = {
-        "agencia": "0001",
-        "numero_conta": 1,
-        "cpf_dono": "01174820306",
-        "LIMITE_SAQUE": 500.00,
-        "SAQUES_DISPONIVEIS": 3,
-        "cont_saques": 0,
-        "saldo": 0.0,
-        "extrato": "",
-    }
-
-    nova_conta["saldo"] = 100
-
-    clientes.append(novo_cliente)
-    clientes[0]["contas"].append(nova_conta)
-    contas.append(nova_conta)
-
-    menu_principal(clientes, contas)
+#########################################################
 
 
-main()
+#########################################################
+#   INÍCIO DA EXECUÇÃO
+#########################################################
+
+selected_option = -1
+account_balance = 0
+statement = ""
+
+while True:
+    print_main_menu()
+
+    # print(opcao_selecionada)
+    selected_option = input("   Selecione a opção desejada: ")
+
+    if selected_option == "0":
+        deposit_value()
+
+    elif selected_option == "1":
+        take_value()
+
+    elif selected_option == "2":
+        print_statement()
+
+    elif selected_option == "9":
+        exit_program()
+
+    else:
+        print("\n   Opção inválida!")
+
+
+#########################################################
+#   FIM DA EXECUÇÃO
+#########################################################
